8013a93e10469e4dfa9c5917eefd3e0e31481d97
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-04 18:39:59 -0500

    Complete the Get requests

---
+++ accounts/accountRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const db = require("../data/dbConfig.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Failed to get accounts from database" });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .where("id", "=", req.params.id)
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Failed to get account from database" });
+    });
+});
+
+
+
+module.exports = router;
