04383d41648464c69619b877b0f63f5f17d2fa69
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-04 18:38:56 -0500

    Set up the server, running successfully

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
+const accountRoutes = require('./accounts/accountRoutes.js')
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', accountRoutes);
+
+server.get('/', (req, res) => {
+  res.send('<h1>Welcome to Accounts API</h1>')
+})
 
 module.exports = server;
